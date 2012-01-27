1b5776fc5b02f4c5f73805aeae029b9c10c03b9d
Henrik Jepsen <hekjje@gmail.com>
2012-01-27 11:14:36 +0100

    Adding google analytics
    Form the theme settings page you can controle your google analytics settings

--- html.tpl.php
+++ html.tpl.php
@@ -18,6 +18,25 @@
 				});
 			});
 		</script>
+		<?php if(theme_get_setting('ga_use') and theme_get_setting('ga_ua')){
+			if($user->uid == 0 or theme_get_setting('ga_login')){
+				?>
+				<script type="text/javascript">
+
+				  var _gaq = _gaq || [];
+				  _gaq.push(['_setAccount', '<?= theme_get_setting('ga_ua') ?>']);
+				  _gaq.push(['_trackPageview']);
+
+				  (function() {
+				    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				  })();
+
+				</script>
+				<?php	
+			}
+		} ?>
 	</head>
 	<body class="<?= $classes ?>" <?= $attributes ?>>
 		<?= $page_top ?>
---
+++ theme-settings.php
@@ -0,0 +1,23 @@
+<?php
+function bootstrap_form_system_theme_settings_alter(&$form, &$form_state) {
+	$form['bootstrap_settings']['google'] = array(
+	    '#type' => 'fieldset',
+	    '#title' => t('Google Analytics settings')
+	   );
+	$form['bootstrap_settings']['google']['ga_use'] = array(
+		'#type' => 'checkbox',
+		'#title' => t('Use Google analytics'),
+		'#default_value' => theme_get_setting('ga_use'),
+	);
+	$form['bootstrap_settings']['google']['ga_ua'] = array(
+		'#type' => 'textfield',
+		'#title' => t('UA id'),
+		'#description'   => t("Add the Google Analytics id (UA-1234567-8) to add analytics to the theme"),
+		'#default_value' => theme_get_setting('ga_ua'),
+	);
+	$form['bootstrap_settings']['google']['ga_login'] = array(
+		'#type' => 'checkbox',
+		'#title' => t('Use analytics on users how is logged in'),
+		'#default_value' => theme_get_setting('ga_login'),
+	);
+}
